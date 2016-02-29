a0416637dfaf790b4457beda06fba57510f2cf0c
Arief Adjie Wicaksono <id.ariefadjie@gmail.com>
2016-02-29 12:15:23 +0700

    update routes.php
    redirect / to /guestbook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@ use App\Guestbook;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('guestbook');
 });
 Route::get('/guestbook', function(){
 	$guestbooks	= Guestbook::paginate(5);
